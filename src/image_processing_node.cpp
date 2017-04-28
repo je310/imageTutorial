acc9f903b92e579ce0cbc3a89dd3bb234a486650
Your Name <je310@imperial.ac.uk>
2017-04-28 21:47:57 +0100

    Made the code work on all computers, added output of left / right for turning the robot from another node

--- src/image_processing_node.cpp
+++ src/image_processing_node.cpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <std_msgs/Float32.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -17,29 +19,49 @@
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg);
 bool lor(cv::Mat image);
+float lorFloat(cv::Mat image);
+
+//global publisher, as we need to use it in the callback function
+ros::Publisher turnPub;
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "image_processing_node");
 
+    //make strings containing the paths to the test images.
+    std::string path = ros::package::getPath("imageTutorial"); // this line finds where this package is on your computer without using /philip etc
+    std::stringstream ssLeft, ssRight;
+    ssLeft << path << "/src/left.jpg";      //here we just add the ending of the path.
+    ssRight << path << "/src/right.jpg";    //using a stringstream is very similar to using std::cout !
+
+    //do a check to make sure that ros found the folder on the computer.
+    if(path.empty()){
+        std::cout << "I do not know where imageTutorial is..." << std::endl;
+        return -1;
+    }
 
     //set up ros objects
     ros::NodeHandle node;
     image_transport::ImageTransport it_(node);
+    turnPub = node.advertise<std_msgs::Float32>("/turn",1);
 
+    //Subscribe to the thermal image topic.
     image_transport::Subscriber imSub = it_.subscribe("/thermal",1, imageCallback);
 
     //test lor function
     cv::Mat left, right;
-    left = cv::imread("/home/philip/catkin_ws/src/imageTutorial/src/left.jpg");
-    right = cv::imread("/home/philip/catkin_ws/src/imageTutorial/src/right.jpg");
-    cv::imshow("left", left);
-    cv::imshow("right", right);
-    cv::waitKey(1);
+    left = cv::imread(ssLeft.str());
+    right = cv::imread(ssRight.str());
     bool leftTest = lor(left);
     bool rightTest = lor(right);
+    if(leftTest == false && rightTest == true) std::cout << "passed our LoR test" << std::endl;
+
+    //test lorFloat function
+    float leftFloat = lorFloat(left);
+    float rightFloat = lorFloat(right);
+    std::cout << leftFloat << " "  << rightFloat << std::endl;
+    if(leftFloat < 0 && rightFloat > 0) std::cout << "passed our LoRFloat test" << std::endl;
 
     while(ros::ok()){
-
         ros::spinOnce();
     }
 
@@ -47,17 +69,29 @@ int main(int argc, char** argv){
 }
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg){
+
+    //get the image from the message
     cv::Mat image =  cv_bridge::toCvShare(msg, "bgr8")->image;
+
+    //find if the warmer side is left or right
     bool turn = lor(image);
+
+    // if right do ..., else if left do...
+    std_msgs::Float32 turningMessage;
     if(turn){
         std::cout << "right" << std::endl;
-
+        turningMessage.data = 1.0;
     }
     else{
         std::cout << "left" << std::endl;
+        turningMessage.data = -1.0;
     }
-    cv::waitKey(1);
+    //turningMessage.data = lorFloat(image);
+
+    //publish a float number for another node to use to control the robot.
+    turnPub.publish(turningMessage);
 }
+
 bool lor(cv::Mat image){
     //True = right, False = left
     cv::Mat gray;
@@ -85,4 +119,28 @@ bool lor(cv::Mat image){
     else{
         return true;
     }
+}
+
+float lorFloat(cv::Mat image){
+    //positive right, negative left
+    cv::Mat gray;
+    cv::cvtColor(image, gray, CV_BGR2GRAY);
+    int left = 0;
+    int right = 0;
+    // add pixels into 2 bins: left and right.
+    for(int i = 0; i<image.cols; i++){
+        for(int j = 0; j<image.rows; j++){
+            int value = gray.at<uchar>(j, i);
+            if(i<gray.cols/2){
+                left += value;
+
+
+            }
+            else{
+                right+=value;
+            }
+        }
+    }
+    float total = left + right;
+    return (right - left)/ total;
 }
